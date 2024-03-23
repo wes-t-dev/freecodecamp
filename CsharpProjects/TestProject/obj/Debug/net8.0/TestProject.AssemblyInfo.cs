588b2ceaedfb0fa9c6bd0e97d25fece7e58bc158
westdev <west.dev.mail@gmail.com>
2024-03-22 20:34:58 -0500

    update

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa10dc41184574542b8960106c9cf851a8eb088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0c2ed3b61460c47e6b39ce13260b3029483d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
